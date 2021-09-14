5ff164af9541fbbf3044a851def8cb4cdbb898b9
deangelacgn@gmail.com <deangelacgn@gmail.com>
2021-09-14 09:34:58 -0300

    Project setup

--- searchvid/asgi.py
+++ searchvid/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for tedfinder project.
+ASGI config for searchvid project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tedfinder.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'searchvid.settings')
 
 application = get_asgi_application()
--- searchvid/wsgi.py
+++ searchvid/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for tedfinder project.
+WSGI config for searchvid project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tedfinder.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'searchvid.settings')
 
 application = get_wsgi_application()
